a1f0c583efd25cf64c47b19edaee395dad59612a
Shughla <nikusha1702@outlook.com>
2021-08-25 04:40:12 +0400

    Add run onfiguration, variable passing to js

--- SwitcherWebServer/app.py
+++ SwitcherWebServer/app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request
 from Switcher.switcher import Switcher
+
 app = Flask(__name__)
 
 # main run, runs switcher first, if can't connect doesn't run server
 if __name__ == "__main__":
-    switcher = Switcher("junior", "project")
-    logger = switcher.logger
-    logger.append(logger.get_time() + "Server started.")
-    switcher.run()
+    # switcher = Switcher("junior", "project")
+    # logger = switcher.logger
+    # logger.append(logger.get_time() + "Server started.")
+    # switcher.run()
     app.run()
+
+
+def render_index():
+    data = {'name': 'სახელი', 'surname': 'გვარი'}
+    return render_template("index.html", data=data)
 
 
 @app.route('/')
 def main_page():
-    return render_template("index.html")
+    return render_index()
 
 
 # with app.app_context('/'):
@@ -24,7 +30,7 @@ def switch():
     dct = data.to_dict()
     print(dct.get("num"))
     # print(dct.get("state"))
-    return render_template("index.html")
+    return render_index()
 
 
 @app.route('/switch', methods=['GET'])
