4e5666d9364166d4f5ef22e869b97dbfbca84048
Shughla <nikusha1702@outlook.com>
2021-09-03 23:09:29 +0400

    Add sensor code

--- esp32code/esp32code.ino
+++ esp32code/esp32code.ino
@@ -5,27 +5,34 @@
   WiFiClient espClient;
   PubSubClient client(espClient);
 
-
+  const char TOPIC[9] = "switcher";
+  const int TOTAL_NUMBER = 5;
   // servo motors and their working pins
   Servo servo1;  
   Servo servo2;
   Servo servo3;
   Servo servo4;
   Servo servo5;
-
+  
+  Servo servos[5] = {servo1,servo2,servo3,servo4,servo5};
+  
   const int servoPin1 = 13; 
   const int servoPin2 = 12;
   const int servoPin3 = 14;
   const int servoPin4 = 27;
   const int servoPin5 = 26;
-
+  
+  int servoPins[5] = {servoPin1,servoPin2,servoPin3,servoPin4,servoPin5};
+  
   // sensor pins
   int sens1 = 33;
   int sens2 = 32;
   int sens3 = 35;
   int sens4 = 34;
   int sens5 = 39;
-
+  
+  int sensors[] = {sens1,sens2,sens3,sens4,sens5};
+  
   // the led which is turn on when esp is connected to the broker
   int brokerLed = 15;
 
@@ -58,27 +65,36 @@
       ESP32PWM::allocateTimer(1);
       ESP32PWM::allocateTimer(2);
       ESP32PWM::allocateTimer(3);
-      servo1.setPeriodHertz(50);
-      servo2.setPeriodHertz(50);
-      servo3.setPeriodHertz(50);
-      servo4.setPeriodHertz(50);
-      servo5.setPeriodHertz(50);    
-      servo1.attach(servoPin1); 
-      servo2.attach(servoPin2);
-      servo3.attach(servoPin3);
-      servo4.attach(servoPin4);
-      servo5.attach(servoPin5);
+      for(int i = 0;i < TOTAL_NUMBER;i++){
+        servos[i].setPeriodHertz(50);
+        servos[i].attach(servoPins[i]); 
+      }
   }
 
 
   void setUpSensors(){
-    pinMode(sens1, INPUT);
-    pinMode(sens2, INPUT);
-    pinMode(sens3, INPUT);
-    pinMode(sens4, INPUT);
-    pinMode(sens5, INPUT);  
+    for(int i = 0;i<TOTAL_NUMBER;i++){
+      pinMode(sensors[i],INPUT);
+    }
   }
 
+  boolean readSensor(int n){
+    double val  = analogRead(sensors[n]);
+    return val < 20;
+  }
+  
+    void sendSensorData(){
+    for(int i = 0;i<TOTAL_NUMBER;i++){
+      String st = String(i);
+      if(readSensor(i)){
+        st = st + ":1";
+        client.publish(TOPIC,i + ":1");
+      }else{
+        st = st + ":0";
+        client.publish(TOPIC,i + ":0");
+      }
+    }
+  }
   
   void reconnectToBroker(){
       while (!client.connected()) {
@@ -111,24 +127,31 @@
       memcpy(p, payload, length);
       p[length] = NULL;
       String message(p);
-      String topicName(topic);
-  
-      if(topicName == "room1"){
-          if(message == "0"){
-            turnOffUpperServo();
-          }else{
-            turnOnUpperServo();
-          }
-      }else if(topicName == "room2"){
-          if(message == "0"){
-            turnOffLowerServo();
-          }else{
-            turnOnLowerServo();
-          }
+      if(message == "check"){
+            sendSensorData();
+        return;
       }
+      
+      int index = message.substring(0,message.length()-2).toInt();
+      int value = (message[message.length() - 1] - '0');
+
+      if(index%2 == 0){
+          if(value == 0){
+            turnOffUpperServo(index);
+          }else{
+            turnOnUpperServo(index);
+          }
+      }else{
+         if(value == 0){
+            turnOffLowerServo(index);
+         }else{
+            turnOnLowerServo(index);
+         }
+      }
+      
   }
   
-   
+
   void setup(){
       // setup serial and all working pins on esp
       Serial.begin(115200);
@@ -151,8 +174,7 @@
       reconnectToBroker(); 
   
       // subrscribe all topics
-      client.subscribe("room1");
-      client.subscribe("room2");
+      client.subscribe(TOPIC);
   }
   
    
@@ -162,35 +184,35 @@
 
 
  ////////////////// temproray methods but servo's angles are right ////////////////////
-  void turnOnUpperServo(){
+  void turnOnUpperServo(int i){
        Serial.println("turn on upper");
-       servo1.write(105);
+       servos[i].write(105);
        delay(500);
-       servo1.write(40);
+       servos[i].write(40);
        delay(500);
-       servo1.write(80);
+       servos[i].write(80);
   }
 
 
-  void turnOffUpperServo(){
+  void turnOffUpperServo(int i){
        Serial.println("turn off upper");
-       servo1.write(105);
+       servos[i].write(105);
   }
 
 
 
-  void turnOnLowerServo(){
+  void turnOnLowerServo(int i){
        Serial.println("turn on lower");
-       servo2.write(65);
+       servos[i].write(65);
        delay(500);
-       servo2.write(140);
+       servos[i].write(140);
        delay(500);
-       servo2.write(100);
+       servos[i].write(100);
   }
 
 
-  void turnOffLowerServo(){
+  void turnOffLowerServo(int i){
        Serial.println("turn off lower");
-       servo2.write(65);
+       servos[i].write(65);
   }
   
